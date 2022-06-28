08681015b913dc68ec538ef2155ef29f30c2a55a
DESKTOP-FC2KDHJ\raeu_ <erapropath@gmail.com>
2022-06-28 21:54:26 +0200

    Create MyFirstClass, JavaSyntaxErrors and JavaComments class with examples

---
+++ src/com/erapropath/beginner/lesson01/JavaComments.java
@@ -0,0 +1,21 @@
+package com.erapropath.beginner.lesson01;
+
+public class JavaComments {
+    /**
+     * This is javadoc comment
+     * @param args - an array of strings
+     * @return void - nothing
+     */
+    public static void main(String[] args) {
+        // This is a line comment
+        // System.out.println("Hello world!"); // Hello comment :D
+        /* block comment
+        with multiple lines
+         */
+        /* small comment */System.out.println("This is my first Java program!");
+        System.out.println("Now I'm a Java programmer :D");
+        /* block comment
+        // with inline comment is allowed
+         but with nested block comment not */
+    }
+}
---
+++ src/com/erapropath/beginner/lesson01/MyFirstClass.java
@@ -0,0 +1,10 @@
+package com.erapropath.beginner.lesson01;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java program!");
+        System.out.println("Now I'm a Java programmer :D");
+    }
+}
